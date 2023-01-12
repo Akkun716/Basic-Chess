4980fef55bdf5f45897831138afd58e5ff5a1035
Adon Anglon <adon.anglon@gmail.com>
2023-01-12 09:44:45 -0800

    Added user file input

--- ChessDriver.java
+++ ChessDriver.java
@@ -8,40 +8,56 @@ public class ChessDriver
         ChessPiece piece;
         boolean again = true;
 
-        try
-        {
-            board.inputRead("input.txt");
+        /* User can enter a specific file to view result */
+        if(args.length != 0) {
+            for(String file: args) {
+                try
+                {
+                    board.inputRead(file);
+                }
+                catch(FileNotFoundException e)
+                {
+                    System.out.printf("[File %s was not found]", file);
+                }
+
+                board.clearBoard();
+                System.out.println(board);
+            }
+        } else {
+        /* Default runner if no files are inputed */
+            try
+            {
+                board.inputRead("input.txt");
+            }
+            catch(FileNotFoundException e)
+            {
+                System.out.println("[File was not found]");
+            }
+
+            board.clearBoard();
+            System.out.println(board);
+    
+            try
+            {
+                board.inputRead("new_input.txt");
+            }
+            catch(FileNotFoundException e)
+            {
+                System.out.println("[File was not found]");
+            }
+
+
+            board.clearBoard();
+
+            try
+            {
+                board.inputRead("my_input.txt");
+            }
+            catch(FileNotFoundException e)
+            {
+                System.out.println("[File was not found]");
+            }
+            System.out.println();
         }
-        catch(FileNotFoundException e)
-        {
-            System.out.println("[File was not found]");
-        }
-
-        board.clearBoard();
-        System.out.println(board);
-
-        try
-        {
-            board.inputRead("new_input.txt");
-        }
-        catch(FileNotFoundException e)
-        {
-            System.out.println("[File was not found]");
-        }
-
-
-        board.clearBoard();
-
-        try
-        {
-            board.inputRead("my_input.txt");
-        }
-        catch(FileNotFoundException e)
-        {
-            System.out.println("[File was not found]");
-        }
-        System.out.println();
-
-
     }
 }
--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-# project4
-Basic Chess Game
+# Basic Chess Game
 
-I am unsure if we were supposed to be able to read the new_input.txt file and move the pieces when the piece
-name was displayed. I changed them to simply "move" like the previous file.
+This was a project from my CS112 course that focused on exception handling and validation of moves for chess pieces. There are changes made to the project after its submission, either due to commenting and to enhance readibility and understandability or functional to build upon the project and enhance its capabilities.
+
+##Input Files
+
+The program is currently run using text files that specify chess pieces to be populated and pieces to be moved within the current instance of the board. Pieces are create with the following command: `[piece] [row] [column] [color]`, with row and column 0 beginning from the "top left" of the board and colors black and white represented with the int values 1 and 2. 
+
+## Running program
+
+The ChessDriver class is the main runner of the project, executed simply by compiling the file with `javac ChessDriver.java` and execution with `java ChessDriver [file 1] ...`. If no files are inputed by user, the program will default to running input.txt, new_input.txt, and my_input.txt.
